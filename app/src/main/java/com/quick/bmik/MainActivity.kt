870f8d0df0a7fcebae313751960988f0876c4c40
Simon Wang <quick119@hotmail.com>
2018-10-23 17:45:52 +0800

    單元39

---
+++ app/src/main/java/com/quick/bmik/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.quick.bmik
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        help.setOnClickListener { AlertDialog.Builder(this@MainActivity)
+            .setMessage("The body mass index (BMI) or Quetelet index is a value derived from the mass (weight) and height of an individual.")
+            .setPositiveButton("OK"){dialogInterface, i ->
+                Toast.makeText(this, "Testing", Toast.LENGTH_LONG).show()
+            }.show()}
+        bmi.setOnClickListener {
+            val w = weight.text.toString().toFloat();
+            val h = height.text.toString().toFloat();
+            val bmi = w / (h*h);
+            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+            intent.putExtra("BMI", bmi)
+            startActivity(intent);
+        }
+    }
+}
